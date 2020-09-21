6578ccfecf73b545c92901c77899632c4743c48f
Antal Matyas <antalmatyas9@gmail.com>
2020-09-21 11:21:33 +0300

    created the phone agenda web with css and html

---
+++ js/contacts.js
@@ -0,0 +1,18 @@
+//This is like a class in JAVA
+window.PhoneAgenda={
+
+    //Variable to stock our url
+    //all our request will do on this url
+    API_URL:"http://localhost:8082/contacts",
+
+
+    //Creating a function respecting JSON syntax(name and value)
+    createTask:function (){
+
+        //Now we send a request on the server
+        $.ajax()
+
+
+    }
+
+};
